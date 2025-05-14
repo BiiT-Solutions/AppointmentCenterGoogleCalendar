0c756b7df51821295ba99ef05aa849d1be094c64
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-05-14 21:26:08 +0200

    Added token test

---
+++ src/test/java/com/biit/appointment/google/client/TokenTests.java
@@ -0,0 +1,34 @@
+package com.biit.appointment.google.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.UUID;
+
+@SpringBootTest
+@Test(groups = {"tokenTests"})
+public class TokenTests extends AbstractTestNGSpringContextTests {
+
+    private UUID userId = UUID.fromString("5566b76f-cc42-4304-b28e-36c6ef6ddc6a");
+    private String token = "4/0AUJR-x4yIAjVMeFggCn1tpsD6WHXXKXjSwqYg7ox23gJA4iRJhVNIw8E1o4vQQNEuTf3qw";
+
+    @Value("${google.client.state:}")
+    private String clientState;
+
+    @Autowired
+    private GoogleCalendarController googleCalendarController;
+
+    @Test
+    public void convertCredentials() {
+        googleCalendarController.exchangeCodeForToken(userId, token, clientState, null);
+    }
+
+
+}
