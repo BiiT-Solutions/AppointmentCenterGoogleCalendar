41530b7861f3508343529ec03b04f5f44d469d06
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-05-14 12:06:30 +0200

    Fixed param

--- src/main/java/com/biit/appointment/rest/api/GoogleServices.java
+++ src/main/java/com/biit/appointment/rest/api/GoogleServices.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.PathParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +38,18 @@ public class GoogleServices {
                                                         @PathVariable(name = "code") String code,
                                                         @Parameter(description = "State that matches the code of the requester.", required = true)
                                                         @PathVariable(name = "state") String state,
+                                                        Authentication authentication,
+                                                        HttpServletRequest request) {
+        return googleCalendarController.exchangeCodeForToken(authentication.getName(), code, state);
+    }
+
+
+    @PreAuthorize("hasAnyAuthority(@securityService.viewerPrivilege, @securityService.editorPrivilege, @securityService.adminPrivilege)")
+    @Operation(summary = "Gets the credentials from a user on a specific provider.", security = @SecurityRequirement(name = "bearerAuth"))
+    @GetMapping(value = "/code", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ExternalCalendarCredentialsDTO getGoogleAuthByRequestParams(@RequestParam(name = "code") String code,
+                                                        @RequestParam(name = "state") String state,
                                                         Authentication authentication,
                                                         HttpServletRequest request) {
         return googleCalendarController.exchangeCodeForToken(authentication.getName(), code, state);
