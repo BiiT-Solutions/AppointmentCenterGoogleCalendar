d0b756e3e417522f83f65b25f3a113c52e3e1573
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-06-03 10:35:00 +0200

    Allowing to define the server protocol

--- src/test/java/com/biit/appointment/google/client/TokenTests.java
+++ src/test/java/com/biit/appointment/google/client/TokenTests.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 public class TokenTests extends AbstractTestNGSpringContextTests {
 
     private UUID userId = UUID.fromString("5566b76f-cc42-4304-b28e-36c6ef6ddc6a");
-    private String token = "4/0AUJR-x4yIAjVMeFggCn1tpsD6WHXXKXjSwqYg7ox23gJA4iRJhVNIw8E1o4vQQNEuTf3qw";
+    private String oauthCode = "4/0AUJR-x5UCDfzFqD9LO3nBA42RPqNH3GKgvShKsW9-1vBrxwcL8d_v3gm6YxFn-Fl3qn97A";
 
     @Value("${google.client.state:}")
     private String clientState;
@@ -21,11 +21,10 @@ public class TokenTests extends AbstractTestNGSpringContextTests {
     @Autowired
     private GoogleCalendarService googleCalendarService;
 
+    /** Not working on localhost server */
     @Test
     public void convertCredentials() {
-        //The token must be generated from the correct app to work this test.
-        googleCalendarService.exchangeCodeForToken(userId, token, clientState, null);
+        //The oauthCode must be generated from the frontend to work with this test.
+        googleCalendarService.exchangeCodeForToken(userId, oauthCode, clientState, null);
     }
-
-
 }
