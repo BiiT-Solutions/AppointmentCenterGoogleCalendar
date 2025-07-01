e31f1628ab8fd02c084d2ee6fa3600dcf490e0ae
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-01 13:17:36 +0200

    Corrected time format

--- src/main/java/com/biit/appointment/google/client/LocalDateTimeAdapter.java
+++ src/main/java/com/biit/appointment/google/client/LocalDateTimeAdapter.java
@@ -11,23 +11,25 @@ import com.google.gson.JsonSerializer;
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+    private static final int DIGITS = 3;
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_INSTANT;
+    private static final DateTimeFormatter ISO_INSTANT = new DateTimeFormatterBuilder().appendInstant(DIGITS).toFormatter();
 
     @Override
     public JsonElement serialize(LocalDateTime localDate,
                                  Type type,
                                  JsonSerializationContext jsonSerializationContext) {
-        return new JsonPrimitive(localDate.format(FORMATTER) + "Z");
+        return new JsonPrimitive(localDate.format(ISO_INSTANT));
     }
 
     @Override
     public LocalDateTime deserialize(JsonElement jsonElement,
                                      Type type,
                                      JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-        return LocalDateTime.parse(jsonElement.getAsJsonPrimitive().getAsString(), FORMATTER);
+        return LocalDateTime.parse(jsonElement.getAsJsonPrimitive().getAsString(), ISO_INSTANT);
     }
 
 }
