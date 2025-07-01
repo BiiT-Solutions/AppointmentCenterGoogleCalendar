049cead0e0cdb166614f4b930964386f4f11d9d0
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-01 15:32:32 +0200

    Corrected time format

--- src/main/java/com/biit/appointment/google/client/LocalDateTimeAdapter.java
+++ src/main/java/com/biit/appointment/google/client/LocalDateTimeAdapter.java
@@ -11,23 +11,29 @@ import com.google.gson.JsonSerializer;
 import java.lang.reflect.Type;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+    private static final int SECONDS_MAX_DECIMALS = 9;
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    private static final DateTimeFormatter FORMATTER = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
+            .appendFraction(ChronoField.MILLI_OF_SECOND, 0, SECONDS_MAX_DECIMALS, true)
+            .toFormatter();
 
     @Override
     public JsonElement serialize(LocalDateTime localDate,
                                  Type type,
                                  JsonSerializationContext jsonSerializationContext) {
-        return new JsonPrimitive(localDate.format(FORMATTER) + "Z");
+        return new JsonPrimitive(localDate.format(FORMATTER));
     }
 
     @Override
     public LocalDateTime deserialize(JsonElement jsonElement,
                                      Type type,
                                      JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-        return LocalDateTime.parse(jsonElement.getAsJsonPrimitive().getAsString().replace("Z", ""), FORMATTER);
+        return LocalDateTime.parse(jsonElement.getAsJsonPrimitive().getAsString(), FORMATTER);
     }
 
 }
