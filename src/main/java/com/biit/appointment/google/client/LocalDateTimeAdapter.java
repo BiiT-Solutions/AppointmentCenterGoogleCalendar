f032a574ed06ec74cbddaf67bac3efda1d308e1c
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-01 12:36:48 +0200

    Corrected time format

--- src/main/java/com/biit/appointment/google/client/LocalDateTimeAdapter.java
+++ src/main/java/com/biit/appointment/google/client/LocalDateTimeAdapter.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
     @Override
     public JsonElement serialize(LocalDateTime localDate,
